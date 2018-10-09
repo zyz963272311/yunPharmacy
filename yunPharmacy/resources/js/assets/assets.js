e881a5875f97d505c6532089449599be33cefe8d
zyz963272311 <963272311@qq.com>
2018-10-09 23:19:53 +0800

    主页与列表页重构

--- yunPharmacy/resources/js/assets/assets.js
+++ yunPharmacy/resources/js/assets/assets.js
@@ -42,6 +42,73 @@ function getDrugsDetail(classification,deugsName)
 		return null;
 	}
 }
+//获取根路径
+function getRootUrl()
+{
+	var href = window.location.href;
+	var idx = href.indexOf("?");
+	if(idx)
+	{
+		return href.substring(0,idx);
+	}
+	return href;
+}
+function reBuildUrl(params)
+{
+	var name;
+	for(var tempname in params)
+	{
+		name = tempname;
+		break;
+	}
+	var value = params[name];
+	var oldValue = getUrlParam(name);
+	var href = window.location.href;
+	if(oldValue)
+	{
+		href = href.replace(name+"="+oldValue,name+"="+value);
+	}
+	else
+	{
+		var idx = href.indexOf("?");
+		if(idx>0)
+		{
+			href = href+"&"+name+"="+value;
+		}
+		else
+		{
+			href = href+"?"+name+"="+value;
+		}
+	}
+	return href;
+}
+//获取参数
+function getUrlParams()
+{
+	var href = window.location.href;
+	var idx = href.indexOf("?");
+	var result;
+	if(idx>0)
+	{
+		var params = href.substring(idx+1);
+		if(params)
+		{
+			result={};
+			var paramArray = params.split("&");
+			for(var i = 0;i<paramArray.length;i++)
+			{
+				var paramKV = paramArray[i];
+				var paramArray = paramKV.split("=");
+				var name = paramArray[0];
+				var value = paramArray[1];
+				result[name] = value;
+			}
+			return result;
+		}
+	}
+	return {};
+}
+//获取某一个参数值
 function getUrlParam(name) {
     //构造一个含有目标参数的正则表达式对象
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); 
@@ -51,4 +118,50 @@ function getUrlParam(name) {
     if (r != null) return unescape(r[2]);
     //不存在时返回null
     return null; 
+}
+//点击页面
+function clickHeadButton(cls)
+{
+	window.location.href="classification.html?class="+cls;
+}
+//翻页点击
+function aClick(param)
+{
+	var page=param;
+	if(param ==="page1")
+	{
+		//向后翻页
+		page = getUrlParam("page");
+		if(page)
+		{
+			var intpage=Number(page);
+			if(intpage<2)
+			{
+				intpage++;
+			}
+			page=intpage+"";
+		}
+		else{
+			page = "1";
+		}
+	}
+	if(param ==="page_1")
+	{
+		//向前翻页
+		page = getUrlParam("page");
+		if(page)
+		{
+			var intpage=Number(page);
+			if(intpage>1)
+			{
+				intpage--;
+			}
+			page=intpage+"";
+		}
+		else{
+			page = "1";
+		}
+	}
+	var params={"page":page};
+	window.location.href = reBuildUrl(params);
 }
